281770094d102ab272e5fce1a70ea31a359e170a
DeepuGagan <gagandeepu717@gmail.com>
2025-03-21 20:58:28 +0530

    added smooth scrolling to last message, options of chatgpt models, typing indicator

--- src/util/chatgpt.js
+++ src/util/chatgpt.js
@@ -1,13 +1,25 @@
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const axios = require('axios').default
 
-// const chatgptAPI = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
+const chatgptTurbo = async (prompt) => {
+	const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
+	const requestBody = {
+		"model": "gpt-3.5-turbo",
+		"messages": [{ "role": "assistant", "content": `${prompt}` }]
+	}
 
-const generateChatGPTRes = async (prompt) => {
-	const completion = await openai.createCompletion({
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+	}
+
+	const completion = await axios.post(API_ENDPOINT, requestBody, {headers})
+	return completion.data.choices[0].message.content
+}
+
+const chatgptDavinci = async (prompt) => {
+	prompt = prompt.includes("hi") || prompt.includes("Hi") ? prompt :  `Which 5 instyle magazine article links do you recommend on "${prompt}"`
+	const API_ENDPOINT = 'https://api.openai.com/v1/completions'
+	const requestBody = {
 		model: "text-davinci-003",
 		prompt,
 		temperature: 1,
@@ -15,12 +27,21 @@ const generateChatGPTRes = async (prompt) => {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-	});
-	const openAiResponses = completion.data.choices.reduce((acc, { text }) => ({ ...acc, text: text.replace("\n", "") }), {})
-	console.log('completion : ', prompt, ' res: ', openAiResponses)
-	return JSON.stringify(openAiResponses)
+	}
+
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+	}
+
+	const completion = await axios.post(API_ENDPOINT, requestBody, {headers})
+	return completion.data.choices[0].text
 }
 
-module.exports = {
-	generateChatGPTRes
+const generateChatGPTRes = async (prompt) => {
+	const openAiResponses = await chatgptDavinci(prompt)
+	// console.log('completion : ', prompt, ' res: ', openAiResponses)
+	return openAiResponses
 }
+
+module.exports = {generateChatGPTRes}
